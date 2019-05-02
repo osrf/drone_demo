fd89d3d070f163e4b8a49d3178dc434e5ab22bd9
Tully Foote <tfoote@osrfoundation.org>
2019-05-01 17:02:11 -0700

    switch to use rocker for run command

--- run.bash
+++ run.bash
@@ -5,36 +5,6 @@ IMG=tfoote/drone_demo
 
 ARGS=("$@")
 
-# Make sure processes in the container can connect to the x server
-# Necessary so gazebo can create a context for OpenGL rendering (even headless)
-XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
-fi
-
-DOCKER_OPTS=
+rocker --x11 --nvidia --user --home --pulse  $IMG ${ARGS[@]}
 
 
-
-# --device=/dev/input/js0 \
-# --tmpfs /ccache:rw,noexec,nosuid,size=2000000k \
-docker run -it \
-  -e DISPLAY \
-  -e QT_X11_NO_MITSHM=1 \
-  -e XAUTHORITY=$XAUTH \
-  -v "$XAUTH:$XAUTH" \
-  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-  -v "/etc/localtime:/etc/localtime:ro" \
-  --rm \
-  --runtime=nvidia \
-  --security-opt seccomp=unconfined \
-  $IMG ${ARGS[@]}
-  
