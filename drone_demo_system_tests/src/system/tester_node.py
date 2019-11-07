a4881d356df0d858361e322fa83c556ded6b33ca
ahcorde <ahcorde@gmail.com>
2019-11-07 17:26:16 +0100

    Update test to use action and removed px4_msgs dependency

--- drone_demo_system_tests/src/system/tester_node.py
+++ drone_demo_system_tests/src/system/tester_node.py
@@ -18,11 +18,17 @@ import math
 import numpy
 import sys
 import time
+import numpy as np
 
-from px4_msgs.msg import VehicleCommand, VehicleGpsPosition
-from px4_msgs.msg import VehicleStatus, VehicleOdometry, VehicleLandDetected
+from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import PoseStamped
+from action_msgs.msg import GoalStatus
+from proposed_aerial_msgs.action import SetFlightMode
+from proposed_aerial_msgs.msg import FlightMode
+
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 
 import pymap3d as pm
 
@@ -40,114 +46,105 @@ class DroneTester(Node):
 
         print("pose to reach: ", self.goal_pose)
 
-        self.vehicle_command_pub = self.create_publisher(VehicleCommand,
+        self.set_flight_mode_action_client = ActionClient(self, SetFlightMode, 'set_flight_mode')
+
+        self.action_response_received = False
+        self.action_response_success = False
+        self.action_response_result = None
+
+        self.vehicle_command_pub = self.create_publisher(PoseStamped,
                                                          self.get_namespace() +
-                                                         '/vehicle_command',
+                                                         '/command_pose',
                                                          1)
 
-        self.vehicle_status_sub = self.create_subscription(VehicleStatus,
+        self.model_pose_sub = self.create_subscription(NavSatFix,
                                                        self.get_namespace() +
-                                                       '/vehicle_status',
-                                                       self.statusCallback, 10)
-
-        self.model_pose_sub = self.create_subscription(VehicleGpsPosition,
-                                                       self.get_namespace() +
-                                                       '/vehicle_gps_position',
+                                                       '/gps',
                                                        self.poseCallback, 10)
 
-        self.vehicle_odometry_sub = self.create_subscription(VehicleOdometry,
+        self.flight_mode_sub = self.create_subscription(FlightMode,
                                                        self.get_namespace() +
-                                                       '/vehicle_odometry',
-                                                       self.odometryCallback, 10)
-
-        self.vehicle_land_sub = self.create_subscription(VehicleLandDetected,
-                                                       self.get_namespace() +
-                                                       '/vehicle_land_detected',
-                                                       self.landCallback, 10)
+                                                       '/flight_mode',
+                                                       self.flightModeCallback, 10)
 
         self.initial_pose_received = False
         self.current_pose = None
 
-        self.arming_state = -1
-        self.relative_alt = 0
+        self.altitude_ref = 0
+
         self.landed = -1
 
-    def arm_vehicle(self, timeout=5, attempts_limit=5):
-        attempt = 0
-        while self.arming_state != VehicleStatus.ARMING_STATE_ARMED:
-            attempt = attempt + 1
-            self.info_msg('Trying to arm vehicle')
-            msg_vehicle_command = VehicleCommand()
-            msg_vehicle_command.timestamp = int(self.get_clock().now().nanoseconds/1000.0)
-            msg_vehicle_command.command = VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM
-            msg_vehicle_command.param1 = 1.0
-            msg_vehicle_command.param2 = 0.0
-            msg_vehicle_command.param3 = 0.0
-            msg_vehicle_command.param4 = 0.0
-            msg_vehicle_command.param5 = 0.0
-            msg_vehicle_command.param6 = 0.0
-            msg_vehicle_command.param7 = 0.0
-            msg_vehicle_command.confirmation = 1
-            msg_vehicle_command.source_system = 255
-            msg_vehicle_command.target_system = self.target_system
-            msg_vehicle_command.target_component = 1
-            msg_vehicle_command.from_external = True
-            print(self.vehicle_command_pub.topic)
-            self.vehicle_command_pub.publish(msg_vehicle_command)
+    def send_goal(self, mode):
+        self.get_logger().info('Waiting for action server...')
+        self.set_flight_mode_action_client.wait_for_server()
 
-            start_time = time.time()
-            while (time.time() - start_time) < timeout:
-                rclpy.spin_once(self, timeout_sec=1)
-            if (attempt > attempts_limit):
-                return False
-        self.info_msg('Vehicle armed')
-        return True
+        goal_msg = SetFlightMode.Goal()
+        goal_msg.goal.flight_mode = mode
 
-    def takeoff_vehicle(self, timeout=5, attempts_limit=5):
-        attempt = 0
-        while not self.initial_pose_received:
+        self.get_logger().info('Sending goal request...')
+
+        self._send_goal_future = self.set_flight_mode_action_client.send_goal_async(
+            goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        status = future.result().status
+        self.action_response_received = True
+        self.action_response_success = result.success
+        self.action_response_result = result.result.flight_mode
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info('Goal succeeded!')
+        else:
+            self.get_logger().info('Goal failed')
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def arm_vehicle(self):
+        self.info_msg("arming vehicle")
+        self.send_goal(FlightMode.FLIGHT_MODE_ARMED)
+        self.action_response_received = False
+
+        while(not self.action_response_received):
             rclpy.spin_once(self, timeout_sec=0.1)
-        while self.relative_alt < 2:
-            attempt = attempt + 1
-            self.info_msg('Trying to takeoff vehicle')
-            msg_vehicle_command = VehicleCommand()
-            msg_vehicle_command.timestamp = int(self.get_clock().now().nanoseconds/1000.0)
-            msg_vehicle_command.command = VehicleCommand.VEHICLE_CMD_NAV_TAKEOFF
-            msg_vehicle_command.param1 = 0.1
-            msg_vehicle_command.param2 = 0.0
-            msg_vehicle_command.param3 = 0.0
-            msg_vehicle_command.param4 = 90.0*3.1416/180
-            msg_vehicle_command.param5 = self.reference_pose[0]
-            msg_vehicle_command.param6 = self.reference_pose[1]
-            msg_vehicle_command.param7 = 3.0
-            msg_vehicle_command.confirmation = 1
-            msg_vehicle_command.source_system = 255
-            msg_vehicle_command.target_system = self.target_system
-            msg_vehicle_command.target_component = 1
-            msg_vehicle_command.from_external = True
-            self.vehicle_command_pub.publish(msg_vehicle_command)
-            start_time = time.time()
-            while (time.time() - start_time) < timeout:
-                rclpy.spin_once(self, timeout_sec=1)
-            if (attempt > attempts_limit):
-                return False
-        self.info_msg('Vehicle flying!')
-        return True
+        self.info_msg("arming vehicle response received")
 
-    def RTL_vehicle(self, timeout=5, attempts_limit=5):
+        return (self.action_response_result == FlightMode.FLIGHT_MODE_ARMED)
+
+    def takeoff_vehicle(self):
+        self.info_msg("takeoff vehicle")
+
+        while(self.current_pose == None):
+            rclpy.spin_once(self, timeout_sec=0.1)
+
+        self.altitude_ref = self.current_pose.altitude
+
+        self.send_goal(FlightMode.FLIGHT_MODE_FLYING)
+        self.action_response_received = False
+
+        while(not self.action_response_received):
+            rclpy.spin_once(self, timeout_sec=0.1)
+        self.info_msg("takeoff vehicle response received")
+
+        return (self.action_response_result == FlightMode.FLIGHT_MODE_FLYING)
+
+    def RTL_vehicle(self):
         self.goal_pose[0] = 0
         self.goal_pose[1] = 0
         self.goal_pose[2] = 0
-        self.info_msg('Trying to return to launch')
-        msg_vehicle_command = VehicleCommand()
-        msg_vehicle_command.timestamp = int(self.get_clock().now().nanoseconds/1000.0)
-        msg_vehicle_command.command = VehicleCommand.VEHICLE_CMD_NAV_RETURN_TO_LAUNCH
-        msg_vehicle_command.confirmation = 1
-        msg_vehicle_command.source_system = 255
-        msg_vehicle_command.target_system = self.target_system
-        msg_vehicle_command.target_component = 1
-        msg_vehicle_command.from_external = True
-        self.vehicle_command_pub.publish(msg_vehicle_command)
+        self.info_msg("RTL vehicle")
+        self.send_goal(FlightMode.FLIGHT_MODE_RTL)
+        self.action_response_received = False
 
     def info_msg(self, msg: str):
         self.get_logger().info('\033[1;37;44m' + msg + '\033[0m')
@@ -158,44 +155,42 @@ class DroneTester(Node):
     def error_msg(self, msg: str):
         self.get_logger().error('\033[1;37;41m' + msg + '\033[0m')
 
+    def euler_to_quaternion(self, yaw, pitch, roll):
+
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+        return [qx, qy, qz, qw]
+
     def setGoalPose(self):
-        ned3 = (self.goal_pose[0], self.goal_pose[1], -self.goal_pose[2])
-        lat, lon, alt = pm.ned2geodetic(*ned3, *self.reference_pose)
-        print("Flying to: ")
-        print(lat, lon, alt)
-        msg_vehicle_command = VehicleCommand()
-        msg_vehicle_command.timestamp = int(self.get_clock().now().nanoseconds/1000.0)
-        msg_vehicle_command.command = VehicleCommand.VEHICLE_CMD_DO_REPOSITION
-        msg_vehicle_command.param1 = -1.0
-        msg_vehicle_command.param2 = 1.0
-        msg_vehicle_command.param3 = 0.0
-        msg_vehicle_command.param4 = float(self.goal_pose[3]*3.1416/180)
-        msg_vehicle_command.param5 = float(lat)
-        msg_vehicle_command.param6 = float(lon)
-        msg_vehicle_command.param7 = float(self.goal_pose[2])
-        msg_vehicle_command.confirmation = 0
-        msg_vehicle_command.source_system = 255
-        msg_vehicle_command.target_system = self.target_system
-        msg_vehicle_command.target_component = 1
-        msg_vehicle_command.from_external = True
-        self.vehicle_command_pub.publish(msg_vehicle_command)
+          msg = PoseStamped();
+          msg.header.stamp = self.get_clock().now().to_msg();
+
+          msg.pose.position.x = self.goal_pose[0]
+          msg.pose.position.y = self.goal_pose[1]
+          msg.pose.position.z = self.goal_pose[2]
+
+          q = self.euler_to_quaternion(-self.goal_pose[3]+1.57, 0, 0)
+
+          msg.pose.orientation.x = q[0]
+          msg.pose.orientation.y = q[1];
+          msg.pose.orientation.z = q[2];
+          msg.pose.orientation.w = q[3];
+
+          self.vehicle_command_pub.publish(msg)
 
     def poseCallback(self, msg):
         self.current_pose = msg
         if(not self.initial_pose_received):
             self.info_msg('Received vehicle_gps_position')
-            self.reference_pose = (msg.lat*1e-7, msg.lon*1e-7, msg.alt*1e-3)
+            self.reference_pose = (msg.latitude, msg.longitude, msg.altitude)
             print(self.reference_pose)
         self.initial_pose_received = True
 
-    def statusCallback(self, msg):
-        self.arming_state = msg.arming_state
-
-    def odometryCallback(self, msg):
-        self.relative_alt = -msg.z
-
-    def landCallback(self, msg):
-        self.landed = msg.landed
+    def flightModeCallback(self, msg):
+        self.landed = msg.flight_mode == FlightMode.FLIGHT_MODE_DISARMED
 
     def reachesGoal(self, timeout, distance):
         goalReached = False
@@ -214,6 +209,21 @@ class DroneTester(Node):
                     self.error_msg('Robot timed out reaching its goal!')
                     return False
 
+    def reachesTakeOffAltitude(self, timeout, distance):
+        altitudeReached = False
+        start_time = time.time()
+
+        while not altitudeReached:
+            rclpy.spin_once(self, timeout_sec=1)
+            if self.distanceTakeOff() > 2.5:
+                altitudeReached = True
+                self.info_msg('*** TakeOff altitude REACHED ***')
+                return True
+            elif timeout is not None:
+                if (time.time() - start_time) > timeout:
+                    self.error_msg('Robot timed out reaching its takeoff altitude!')
+                    return False
+
     def reachesRTL(self, timeout, distance):
         homeReached = False
         start_time = time.time()
@@ -230,9 +240,9 @@ class DroneTester(Node):
                     return False
 
     def distanceFromGoal(self):
-        geodetic_coord = (self.current_pose.lat*1e-7,
-                          self.current_pose.lon*1e-7,
-                          self.current_pose.alt*1e-3)
+        geodetic_coord = (self.current_pose.latitude,
+                          self.current_pose.longitude,
+                          self.current_pose.altitude)
         x, y, z = pm.geodetic2ned(*geodetic_coord, *self.reference_pose)
         d_x = self.goal_pose[0] - x
         d_y = self.goal_pose[1] - y
@@ -241,6 +251,8 @@ class DroneTester(Node):
         self.info_msg('Distance from goal is: ' + str(distance))
         return distance
 
+    def distanceTakeOff(self):
+        return self.current_pose.altitude - self.altitude_ref
 
 def test_RobotMovesToGoal(robot_tester):
     robot_tester.info_msg('Setting goal pose')
@@ -248,17 +260,21 @@ def test_RobotMovesToGoal(robot_tester):
     robot_tester.info_msg('Waiting 60 seconds for robot to reach goal')
     return robot_tester.reachesGoal(timeout=60, distance=0.5)
 
+
 def test_ArmVehicle(robot_tester):
     return robot_tester.arm_vehicle()
 
 
 def test_TakeoffVehicle(robot_tester):
-    return robot_tester.takeoff_vehicle()
+    robot_tester.takeoff_vehicle()
+    return robot_tester.reachesTakeOffAltitude(timeout=60, distance=0.5)
 
 
 def test_RTLVehicle(robot_tester):
+    robot_tester.info_msg('RTL_vehicle')
     robot_tester.RTL_vehicle()
     return robot_tester.reachesRTL(timeout=60, distance=0.5)
+
 
 def run_all_tests(robot_tester):
     # set transforms to use_sim_time
@@ -278,17 +294,6 @@ def run_all_tests(robot_tester):
         robot_tester.error_msg('Test FAILED')
 
     return result
-
-
-def fwd_pose(x, y, z, heading):
-    ned3 = (x, y, -z)
-    reference = (37.7332531, -119.5616378, 2800.4)
-    lat, lon, alt = pm.ned2geodetic(*ned3, *reference)
-    print(reference)
-    print(lat, lon, alt)
-
-    return [lat, lon, alt, heading]
-
 
 def get_testers(args):
     testers = []
