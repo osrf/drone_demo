dedf8a03f4bd33bfff03aed6fdd329c26f84ac14
ahcorde <ahcorde@gmail.com>
2019-11-12 11:21:37 +0100

    Adding use_rviz and use_qgroundcontrol as arguments

--- sitl_launcher/launch/demo.launch.py
+++ sitl_launcher/launch/demo.launch.py
@@ -18,33 +18,63 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import SetEnvironmentVariable
 import sys
+from launch import LaunchService
+from launch_ros import get_default_launch_description
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch import LaunchContext
+from launch.utilities import perform_substitutions, normalize_to_list_of_substitutions
+
+from launch.actions import IncludeLaunchDescription
+from launch import LaunchDescriptionSource
 
 def generate_launch_description():
 
-    args = sys.argv[1:]
-    sitl_world = "yosemite";
-    if "--sitl_world" in args:
-        sitl_world = args[args.index("--sitl_world")+1]
-
-    if(sitl_world=="yosemite"):
-        pkg_shared_directory = 'yosemite_valley'
-        launch_file = 'drone_yosemite.launch.py'
-    elif(sitl_world=="mcmillan"):
-        pkg_shared_directory = 'mcmillan_airfield'
-        launch_file = 'drone_mcmillan.launch.py'
-    elif(sitl_world=="ksql"):
-        pkg_shared_directory = 'ksql_airport'
-        launch_file = 'drone_ksql.launch.py'
-    elif(sitl_world=="baylands"):
-        pkg_shared_directory = 'baylands'
-        launch_file = 'drone_baylands.launch.py'
+    pkg_shared_directory = "yosemite_valley"
+    launch_file = 'drone_yosemite.launch.py'
 
     world_dir = get_package_share_directory(pkg_shared_directory)
     included_launch = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
                 world_dir + '/launch/' + launch_file))
 
-    return LaunchDescription([
-        Node(package='qgroundcontrol', node_executable='qgroundcontrol-start.sh', output='screen'),
-        included_launch,
-    ])
+    use_rviz = LaunchConfiguration('use_rviz')
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='False',
+        description='Whether to start RVIZ')
+
+    use_qgroundcontrol = LaunchConfiguration('use_qgroundcontrol')
+    declare_use_qgroundcontrol_cmd = DeclareLaunchArgument(
+        'use_qgroundcontrol',
+        default_value='False',
+        description='Whether to start QGroundcontrol')
+
+    include_nodes = [included_launch]
+
+    sitl_launcher_dir = get_package_share_directory('sitl_launcher')
+
+    start_rviz_cmd = Node(
+        condition=IfCondition(use_rviz),
+        package='rviz2',
+        node_executable='rviz2',
+        node_name='rviz2',
+        arguments=['-d', sitl_launcher_dir + "/rviz/rviz.rviz"],
+        output='screen')
+
+    start_qgroundcontrol_cmd = Node(
+        condition=IfCondition(use_qgroundcontrol),
+        package='qgroundcontrol',
+        node_executable='qgroundcontrol-start.sh',
+        node_name='qgroundcontrol',
+        output='screen')
+
+    ld = LaunchDescription(include_nodes)
+    ld.add_action(declare_use_rviz_cmd)
+    ld.add_action(start_rviz_cmd)
+    ld.add_action(declare_use_qgroundcontrol_cmd)
+    ld.add_action(start_qgroundcontrol_cmd)
+
+    return ld
